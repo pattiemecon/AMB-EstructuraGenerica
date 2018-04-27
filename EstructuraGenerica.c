3c6c9f30b830318ca318384aa85a06c980aeaa11
pepe <zz@zz.com>
2018-04-27 19:07:50 -0300

    baja y modificacion generica

--- EstructuraGenerica.c
+++ EstructuraGenerica.c
@@ -87,7 +87,7 @@ int eGen_buscarPorId(eGenerica listado[] ,int limite, int id)
 
 
 
-int eGen_mostrarUno(eGenerica parametro)
+void eGen_mostrarUno(eGenerica parametro)
 {
      printf("\n %s - %d - %d",parametro.nombre,parametro.idGenerica,parametro.estado);
 
@@ -138,7 +138,7 @@ int eGen_mostrarListadoConBorrados(eGenerica listado[],int limite)
 int eGen_alta(eGenerica  listado[],int limite)
 {
     int retorno = -1;
-    char nombre[50];
+    //char nombre[50];
     int id;
     int indice;
 
@@ -157,6 +157,46 @@ int eGen_alta(eGenerica  listado[],int limite)
                 strcpy(listado[indice].nombre,"juan ");
                 listado[indice].idGenerica = id;
                 listado[indice].estado = OCUPADO;
+            //}
+        }
+    }
+    return retorno;
+}
+
+int eGen_baja(eGenerica  listado[],int limite,int id)
+{
+    int retorno = -1;
+    int indice;
+
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = -2;
+        indice = eGen_buscarPorId(listado, limite, id);
+        if(indice >= 0)
+        {
+            retorno = 0;
+            listado[indice].estado = LIBRE;
+        }
+    }
+    return retorno;
+}
+
+int eGen_modificacion(eGenerica  listado[],int limite,int id)
+{
+    int retorno = -1;
+    int indice;
+
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = -2;
+        indice = eGen_buscarPorId(listado, limite, id);
+        if(indice >= 0)
+        {
+            //if(!getValidString("Nombre?","Error","Overflow", nombre,50,2))
+            //{
+                retorno = 0;
+                strcpy(listado[indice].nombre,"juan ");
+                listado[indice].idGenerica = id;
             //}
         }
     }
